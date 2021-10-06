7f6d4b52270b5b523360c46f4c085e11da4670ae
Kelminator <kemuel.torres1@upr.edu>
2021-10-06 10:18:13 -0400

    added the missing Controllers

---
+++ SList.API/Controllers/ForumsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SList.API.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SList.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ForumsController : ControllerBase
+    {
+        private IForumAppService _forumAppService;
+
+        public ForumsController(IForumAppService forumAppService)
+        {
+            _forumAppService = forumAppService;
+
+        }
+    }
+}
