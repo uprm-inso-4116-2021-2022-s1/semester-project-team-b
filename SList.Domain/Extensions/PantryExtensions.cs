c58d860ca0e412d9875af7268dc88235f1ae40b5
Kelminator <kemuel.torres1@upr.edu>
2021-10-06 22:20:39 -0400

    added the extension methods for the domain objects

---
+++ SList.Domain/Extensions/PantryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using SList.Domain.Models;
+using SList.Domain.Models.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SList.Domain.Extensions
+{
+    public static class PantryExtensions
+    {
+        public static PantryDTO ToPantryDTO(this Pantry pantry) =>
+            new PantryDTO
+            {
+            };
+
+    }
+}
